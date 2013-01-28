47e366d94f28438e3be376a515659f7368401e66
Joel Lehtonen <joel.lehtonen@c2smartlight.com>
2013-01-28 02:46:08 +0200

    Add credits to Bas van Dijk

--- src/ByteStringDeep.hs
+++ src/ByteStringDeep.hs
@@ -4,6 +4,9 @@ module ByteStringDeep (force) where
 import Control.DeepSeq
 import Data.ByteString.Lazy.Internal
 
+-- Thanks to Bas van Dijk for the idea:
+-- http://www.haskell.org/pipermail/libraries/2011-November/017062.html
+
 instance NFData ByteString where
     rnf Empty       = ()
     rnf (Chunk _ b) = rnf b
